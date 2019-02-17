a89a0fba2b5435d33ce5426839bf3d13d00188ee
pyres01 <2148125115@qq.com>
2019-02-17 14:49:47 +0800

    update database at 14:49 2019-2-17 by pyres

--- src/main/resources/static/static/sql/asflower.sql
+++ src/main/resources/static/static/sql/asflower.sql
@@ -140,7 +140,7 @@ CREATE TABLE `manager` (
   `name` varchar(15) COLLATE utf8_bin NOT NULL COMMENT '管理员账号名',
   `password` varchar(32) COLLATE utf8_bin NOT NULL COMMENT '密码，md5',
   `sex` tinyint(1) NOT NULL DEFAULT '0' COMMENT '性别',
-  `birthday` timestamp NULL DEFAULT NULL COMMENT '出生日期',
+  `birthday` date DEFAULT NULL COMMENT '出生日期',
   `address` varchar(150) COLLATE utf8_bin DEFAULT NULL COMMENT '地址',
   `phone` varchar(11) COLLATE utf8_bin DEFAULT NULL COMMENT '联系号码',
   `email` varchar(100) COLLATE utf8_bin DEFAULT NULL COMMENT '邮箱',
@@ -151,14 +151,14 @@ CREATE TABLE `manager` (
   `rank` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 /*Data for the table `manager` */
 
 insert  into `manager`(`id`,`name`,`password`,`sex`,`birthday`,`address`,`phone`,`email`,`qq`,`wechat`,`isDelete`,`deleteTime`,`rank`) values 
-(1,'pyres02','e10adc3949ba59abbe56e057f20f883e',0,'2019-01-11 17:50:11',NULL,NULL,NULL,NULL,NULL,0,NULL,1),
-(2,'test','123456',0,NULL,NULL,NULL,NULL,NULL,NULL,0,NULL,1),
-(3,'test1','123456',0,'2019-01-11 18:01:02',NULL,NULL,NULL,NULL,NULL,0,NULL,1);
+(1,'pyres02','e10adc3949ba59abbe56e057f20f883e',0,'2019-01-11',NULL,NULL,NULL,NULL,NULL,1,'2019-02-16 18:29:27',1),
+(2,'test001','fcea920f7412b5da7be0cf42b8c93759',0,'2019-01-17','广西河池','12345678901','1234@qq.com','12345678','1234567',0,NULL,1),
+(4,'test002','25d55ad283aa400af464c76d713c07ad',0,'2019-01-18','广西贺州','10001223450','1234@sina.com','98765','wxi456789',0,'2019-01-18 14:38:19',1);
 
 /*Table structure for table `orderitem` */
 
@@ -220,11 +220,24 @@ CREATE TABLE `shop` (
   PRIMARY KEY (`id`,`remark`),
   KEY `typeId` (`typeId`),
   KEY `topicId` (`topicId`),
-  CONSTRAINT `shop_ibfk_1` FOREIGN KEY (`typeId`) REFERENCES `shoptype` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `shop_ibfk_1` FOREIGN KEY (`typeId`) REFERENCES `shopcategory` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
   CONSTRAINT `shop_ibfk_2` FOREIGN KEY (`topicId`) REFERENCES `topic` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 /*Data for the table `shop` */
+
+/*Table structure for table `shopcategory` */
+
+DROP TABLE IF EXISTS `shopcategory`;
+
+CREATE TABLE `shopcategory` (
+  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `name` varchar(15) COLLATE utf8_bin NOT NULL COMMENT '分类名',
+  `remark` varchar(200) COLLATE utf8_bin DEFAULT NULL COMMENT '说明',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `shopcategory` */
 
 /*Table structure for table `shopstand` */
 
@@ -245,20 +258,6 @@ CREATE TABLE `shopstand` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 /*Data for the table `shopstand` */
-
-/*Table structure for table `shoptype` */
-
-DROP TABLE IF EXISTS `shoptype`;
-
-CREATE TABLE `shoptype` (
-  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键id',
-  `name` varchar(15) COLLATE utf8_bin NOT NULL COMMENT '分类名',
-  `pid` tinyint(4) NOT NULL DEFAULT '0' COMMENT '父类id',
-  `remark` varchar(200) COLLATE utf8_bin DEFAULT NULL COMMENT '说明',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-/*Data for the table `shoptype` */
 
 /*Table structure for table `siteinfo` */
 
@@ -306,25 +305,26 @@ CREATE TABLE `user` (
   `memberName` varchar(15) COLLATE utf8_bin NOT NULL COMMENT '会员名',
   `password` varchar(32) COLLATE utf8_bin NOT NULL COMMENT '密码',
   `sex` tinyint(1) NOT NULL DEFAULT '0' COMMENT '性别',
-  `realName` varchar(15) COLLATE utf8_bin DEFAULT NULL COMMENT '真实姓名',
-  `idCard` varchar(18) COLLATE utf8_bin DEFAULT NULL COMMENT '身份证号',
-  `birthday` timestamp NULL DEFAULT NULL COMMENT '出生日期',
+  `birthday` date DEFAULT NULL COMMENT '出生日期',
   `phone` varchar(11) COLLATE utf8_bin DEFAULT NULL COMMENT '号码',
   `email` varchar(100) COLLATE utf8_bin DEFAULT NULL COMMENT '邮箱',
   `qq` varbinary(12) DEFAULT NULL COMMENT 'qq',
   `wechat` varchar(20) COLLATE utf8_bin DEFAULT NULL COMMENT '微信',
   `avatar` varbinary(150) DEFAULT NULL COMMENT '头像',
   `rankId` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '等级',
-  `qrcode` varchar(150) COLLATE utf8_bin NOT NULL COMMENT '二维码',
-  `inviter` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT '邀请人',
   `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '状态',
   `isDelete` tinyint(1) NOT NULL DEFAULT '0' COMMENT '软删除',
+  `joinTime` timestamp NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `rank_id` (`rankId`),
-  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`rankId`) REFERENCES `rank` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  KEY `rank_id` (`rankId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 /*Data for the table `user` */
+
+insert  into `user`(`id`,`nickName`,`memberName`,`password`,`sex`,`birthday`,`phone`,`email`,`qq`,`wechat`,`avatar`,`rankId`,`status`,`isDelete`,`joinTime`) values 
+(1,'小猪猪ww','小猪猪ww','11111111',0,'2019-02-06','11111111111','111111@qq.com','111111','1111111',NULL,2,1,0,'2019-01-20 22:11:26'),
+(3,'吴若尘','若尘','12345678',1,'2019-01-22','17729780297','it.pyres@gmail.com','2148125115','it.pyres','qwrrwqr',0,1,0,'2019-01-22 21:18:55'),
+(4,'测试用户1','测试用户1','12345678',0,'2019-02-03','12345678991','123456@qq.com','123456','123456',NULL,2,1,0,'2019-02-04 10:57:14');
 
 /*Table structure for table `usercoupon` */
 
