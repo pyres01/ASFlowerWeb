acaeed0195715bed142c4a1438f27d6251938758
pyres01 <2148125115@qq.com>
2019-02-25 20:24:47 +0800

    set template for /

--- src/main/java/com/wd/ASFlowerWeb/controller/home/HomeController.java
+++ src/main/java/com/wd/ASFlowerWeb/controller/home/HomeController.java
@@ -12,6 +12,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+	@GetMapping("/")
+	public String webRoot(){
+		return "/home/index";
+	}
+	
 	@GetMapping("/home/index")
 	public String index(){
 		return "/home/index";
